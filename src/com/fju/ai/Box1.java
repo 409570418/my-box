5a1f78feab3f8d84ca14c32d135c8c6367831c69
409570418 <zzz1230626@gmail.com>
2022-04-29 10:54:41 +0800

    solved

---
+++ src/com/fju/ai/Box1.java
@@ -0,0 +1,14 @@
+package com.fju.ai;
+
+
+
+public class Box1 extends Box {
+    public Box1(){
+        super(8,8,8);
+    }
+
+    @Override
+    public String toString() {
+        return "no1";
+    }
+}
